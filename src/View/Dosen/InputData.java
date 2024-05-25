f5ade38bfb716777ff7cfc1e1520bdfb0655401b
royanaditya <151741230+royanaditya@users.noreply.github.com>
2024-05-25 16:02:11 +0700

    Update InputData.java

--- src/View/Dosen/InputData.java
+++ src/View/Dosen/InputData.java
@@ -22,9 +22,9 @@ public class InputData extends JFrame {
     
     JLabel header = new JLabel("Input Dosen");
     JLabel labelInputNama = new JLabel("Nama");
-    JLabel labelInputNIDN = new JLabel("NIDN");
+    JLabel labelInputEmail = new JLabel("Email");
     JTextField inputNama = new JTextField();
-    JTextField inputNIDN = new JTextField();
+    JTextField inputEmail = new JTextField();
     JButton tombolTambah = new JButton("Tambah Dosen");
     JButton tombolKembali = new JButton("Kembali");
 
@@ -38,17 +38,17 @@ public class InputData extends JFrame {
 
         add(header);
         add(labelInputNama);
-        add(labelInputNIDN);
+        add(labelInputEmail);
         add(inputNama);
-        add(inputNIDN);
+        add(inputEmail);
         add(tombolTambah);
         add(tombolKembali);
 
         header.setBounds(20, 8, 440, 24);
         labelInputNama.setBounds(20, 32, 440, 24);
         inputNama.setBounds(18, 56, 440, 36);
-        labelInputNIDN.setBounds(20, 96, 440, 24);
-        inputNIDN.setBounds(18, 120, 440, 36);
+        labelInputEmail.setBounds(20, 96, 440, 24);
+        inputEmail.setBounds(18, 120, 440, 36);
         tombolKembali.setBounds(20, 160, 215, 40);
         tombolTambah.setBounds(240, 160, 215, 40);
         
@@ -96,7 +96,7 @@ public class InputData extends JFrame {
       file ControllerMahasiswa.java dan file PageInputMahasiswa.java 
       berada pada package yang berbeda.
     */
-    public String getInputNIDN() {
-        return inputNIDN.getText();
+    public String getInputEmail() {
+        return inputEmail.getText();
     } 
 }
